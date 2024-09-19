4d7c2bd6289639557b069d3b2e4390f63087b91a
SahanDhanujaya <sahandhanujaya18@gmail.com>
2024-09-19 21:36:40 +0530

    update SQL module

--- SQL/src/main/java/lk/ijse/Select.java
+++ SQL/src/main/java/lk/ijse/Select.java
@@ -5,6 +5,8 @@ import lk.ijse.entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Select {
@@ -13,10 +15,9 @@ public class Select {
         Transaction transaction = session.beginTransaction();
         NativeQuery query = session.createNativeQuery("SELECT * FROM Customer WHERE name = ?");
         query.setParameter(1,"Dhanu");
-        query.addEntity(Customer.class);
-        List<Customer> list = query.list();
-        for (Customer customer : list){
-            System.out.println(customer.getName());
+        List<Object[]> resultList = query.getResultList();
+        for (Object[] customer : resultList){
+            System.out.println(Arrays.toString(customer));
         }
         transaction.commit();
         session.close();
